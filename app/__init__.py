ae602964cdff18b5d39a436dcb9bac82b30ab92c
lzm <923425198@qq.com>
2019-04-28 21:31:50 +0800

    添加lib包

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# @FileName : __init__.py.py
+# @作者 : Liu
+# @日期 : 2019年04月28日
+# @时间 : 21时29分
+
--- app/main.py
+++ app/main.py
@@ -5,3 +5,17 @@
 # @日期 : 2019年04月26日
 # @时间 : 18时23分
 
+import sys
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return '欢迎回家1'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
